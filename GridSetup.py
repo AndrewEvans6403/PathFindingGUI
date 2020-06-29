331f6cf9da64c45e7d4b8720ab7a6241a723d50d
AndrewEvans6403 <andrew.evans.6403@hotmail.com>
2020-06-28 17:02:28 -0700

    Finished implementing game loop

--- GridSetup.py
+++ GridSetup.py
@@ -1,4 +1,4 @@
-# Andrew Evans 
+# Andrew Evans
 import pygame as pg
 vec = pg.math.Vector2
 
@@ -59,19 +59,34 @@ def draw_grid():
         pg.draw.line(screen, LIGHTGRAY, (0, y), (WIDTH, y))
 
 
-running = True
+grid = SquareGrid(GRIDWIDTH, GRIDHEIGHT)
+walls = [(10, 7), (11, 7), (12, 7), (13, 7), (14, 7), (15, 7), (16, 7), (7, 7), (6, 7), (5, 7), (5, 5), (5, 6), (1, 6), (2, 6), (3, 6), (5, 10), (5, 11), (5, 12), (5, 9), (5, 8), (12, 8), (12, 9), (12, 10), (12, 11), (15, 14), (15, 13), (15, 12), (15, 11), (15, 10), (17, 7), (18, 7), (21, 7), (21, 6), (21, 5), (21, 4), (21, 3), (22, 5), (23, 5), (24, 5), (25, 5), (18, 10), (20, 10), (19, 10), (21, 10), (22, 10), (23, 10), (14, 4), (14, 5), (14, 6), (14, 0), (14, 1), (9, 2), (9, 1), (7, 3), (8, 3), (10, 3), (9, 3), (11, 3), (2, 5), (2, 4), (2, 3), (2, 2), (2, 0), (2, 1), (0, 11), (1, 11), (2, 11), (21, 2), (20, 11), (20, 12), (23, 13), (23, 14), (24, 10), (25, 10), (6, 12), (7, 12), (10, 12), (11, 12), (12, 12), (5, 3), (6, 3), (5, 4)]
+for wall in walls:
+    grid.walls.append(vec(wall))
 
+running = True
 while running:
     clock.tick(FPS)
     for event in pg.event.get():
-        if event.type == pg.quit():
+        if event.type == pg.QUIT():
             running = False
         if event.type == pg.KEYDOWN:
             if event.key == pg.K_ESCAPE:
                 running = False
+            if event.key == pg.K_m:
+                # dump the wall list for saving
+                print([(int(loc.x), int(loc.y)) for loc in grid.walls])
+        if event.type == pg.MOUSEBUTTONDOWN:
+            mpos = vec(pg.mouse.get_pos())
+            if event.button == 1:
+                if mpos in grid.walls:
+                    grid.walls.remove(mpos)
+                else:
+                    grid.walls.append(mpos)
+
     pg.display.set_caption("{:.2f}".format(clock.get_fps()))
     screen.fill(DARKGRAY)
     draw_grid()
-    g.draw()
+    grid.draw()
     pg.display.flip()
 
