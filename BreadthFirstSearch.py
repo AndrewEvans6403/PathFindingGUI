940be2742d119dd3ecc870979b25bb1a585a9472
Andrew Evans <andrew.evans.6403@hotmail.com>
2020-06-28 18:55:28 -0700

    Implements BFS

--- BreadthFirstSearch.py
+++ BreadthFirstSearch.py
@@ -1,10 +1,10 @@
 # Andrew Evans
 import pygame as pg
+from os import path
 from collections import deque
-
 vec = pg.math.Vector2
 
-TILESIZE = 40
+TILESIZE = 48
 GRIDWIDTH = 28
 GRIDHEIGHT = 15
 WIDTH = TILESIZE * GRIDWIDTH
@@ -21,18 +21,14 @@ DARKGRAY = (40, 40, 40)
 LIGHTGRAY = (140, 140, 140)
 
 pg.init()
-
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 clock = pg.time.Clock()
-
 
 class SquareGrid:
     def __init__(self, width, height):
         self.width = width
         self.height = height
         self.walls = []
-        # which direction can you travel in from given tile only allowing up down left right
-        # used to calculate neighbors of given node
         self.connections = [vec(1, 0), vec(-1, 0), vec(0, 1), vec(0, -1)]
 
     def in_bounds(self, node):
@@ -41,10 +37,9 @@ class SquareGrid:
     def passable(self, node):
         return node not in self.walls
 
-    def find_neighbors(self, node, ):
+    def find_neighbors(self, node):
         neighbors = [node + connection for connection in self.connections]
-        # walls / edge cases
-        neighbors = filter(self.inbounds, neighbors)
+        neighbors = filter(self.in_bounds, neighbors)
         neighbors = filter(self.passable, neighbors)
         return neighbors
 
@@ -53,49 +48,43 @@ class SquareGrid:
             rect = pg.Rect(wall * TILESIZE, (TILESIZE, TILESIZE))
             pg.draw.rect(screen, LIGHTGRAY, rect)
 
-
 def draw_grid():
     for x in range(0, WIDTH, TILESIZE):
         pg.draw.line(screen, LIGHTGRAY, (x, 0), (x, HEIGHT))
     for y in range(0, HEIGHT, TILESIZE):
         pg.draw.line(screen, LIGHTGRAY, (0, y), (WIDTH, y))
 
-
-# helper used for path dictionary in BFS method because Vectors are unhashable types
-def vec2int(vector):
-    return int(vector.x), int(vector.y)
-
+def vec2int(v):
+    return (int(v.x), int(v.y))
 
 def breadth_first_search(graph, start):
     frontier = deque()
     frontier.append(start)
     path = {}
-    path[start] = None
+    path[vec2int(start)] = None
     while len(frontier) > 0:
-        current_node = frontier.popleft()
-        for nextTile in graph.find_neighbors(current_node):
-            if vec2int(nextTile) not in path:
-                frontier.append(nextTile)
-                path[vec2int(nextTile)] = current_node - nextTile
+        current = frontier.popleft()
+        for next in graph.find_neighbors(current):
+            if vec2int(next) not in path:
+                frontier.append(next)
+                path[vec2int(next)] = current - next
     return path
 
+icon_dir = path.join(path.dirname(__file__), 'icons')
+arrows = {}
+arrow_img = pg.image.load(path.join(icon_dir, 'arrowRight.png')).convert_alpha()
+arrow_img = pg.transform.scale(arrow_img, (50, 50))
+for dir in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+    arrows[dir] = pg.transform.rotate(arrow_img, vec(dir).angle_to(vec(1, 0)))
 
 grid = SquareGrid(GRIDWIDTH, GRIDHEIGHT)
-walls = [(10, 7), (11, 7), (12, 7), (13, 7), (14, 7), (15, 7), (16, 7), (7, 7), (6, 7), (5, 7), (5, 5), (5, 6), (1, 6),
-         (2, 6), (3, 6), (5, 10), (5, 11), (5, 12), (5, 9), (5, 8), (12, 8), (12, 9), (12, 10), (12, 11), (15, 14),
-         (15, 13), (15, 12), (15, 11), (15, 10), (17, 7), (18, 7), (21, 7), (21, 6), (21, 5), (21, 4), (21, 3), (22, 5),
-         (23, 5), (24, 5), (25, 5), (18, 10), (20, 10), (19, 10), (21, 10), (22, 10), (23, 10), (14, 4), (14, 5),
-         (14, 6), (14, 0), (14, 1), (9, 2), (9, 1), (7, 3), (8, 3), (10, 3), (9, 3), (11, 3), (2, 5), (2, 4), (2, 3),
-         (2, 2), (2, 0), (2, 1), (0, 11), (1, 11), (2, 11), (21, 2), (20, 11), (20, 12), (23, 13), (23, 14), (24, 10),
-         (25, 10), (6, 12), (7, 12), (10, 12), (11, 12), (12, 12), (5, 3), (6, 3), (5, 4)]
+walls = [(10, 7), (11, 7), (12, 7), (13, 7), (14, 7), (15, 7), (16, 7), (7, 7), (6, 7), (5, 7), (5, 5), (5, 6), (1, 6), (2, 6), (3, 6), (5, 10), (5, 11), (5, 12), (5, 9), (5, 8), (12, 8), (12, 9), (12, 10), (12, 11), (15, 14), (15, 13), (15, 12), (15, 11), (15, 10), (17, 7), (18, 7), (21, 7), (21, 6), (21, 5), (21, 4), (21, 3), (22, 5), (23, 5), (24, 5), (25, 5), (18, 10), (20, 10), (19, 10), (21, 10), (22, 10), (23, 10), (14, 4), (14, 5), (14, 6), (14, 0), (14, 1), (9, 2), (9, 1), (7, 3), (8, 3), (10, 3), (9, 3), (11, 3), (2, 5), (2, 4), (2, 3), (2, 2), (2, 0), (2, 1), (0, 11), (1, 11), (2, 11), (21, 2), (20, 11), (20, 12), (23, 13), (23, 14), (24, 10), (25, 10), (6, 12), (7, 12), (10, 12), (11, 12), (12, 12), (5, 3), (6, 3), (5, 4)]
 for wall in walls:
     grid.walls.append(vec(wall))
-
-running = True
-
 start = vec(14, 8)
 path = breadth_first_search(grid, start)
-# game loop
+
+running = True
 while running:
     clock.tick(FPS)
     for event in pg.event.get():
@@ -105,7 +94,7 @@ while running:
             if event.key == pg.K_ESCAPE:
                 running = False
             if event.key == pg.K_m:
-                # prints the wall list to console for saving purposes
+                # dump the wall list for saving
                 print([(int(loc.x), int(loc.y)) for loc in grid.walls])
         if event.type == pg.MOUSEBUTTONDOWN:
             mpos = vec(pg.mouse.get_pos()) // TILESIZE
@@ -114,9 +103,20 @@ while running:
                     grid.walls.remove(mpos)
                 else:
                     grid.walls.append(mpos)
+            if event.button == 3:
+                start = mpos
+            path = breadth_first_search(grid, start)
 
     pg.display.set_caption("{:.2f}".format(clock.get_fps()))
     screen.fill(DARKGRAY)
     draw_grid()
     grid.draw()
+    for node, dir in path.items():
+        if dir:
+            x, y = node
+            x = x * TILESIZE + TILESIZE / 2
+            y = y * TILESIZE + TILESIZE / 2
+            img = arrows[vec2int(dir)]
+            r = img.get_rect(center=(x, y))
+            screen.blit(img, r)
     pg.display.flip()
